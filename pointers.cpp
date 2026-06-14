83569787ca580f3e364a65dc2bf8968396b242d7
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 17:54:10 -0500

    adding to x pointer

--- pointers.cpp
+++ pointers.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main(){
     int x = 9;
-    cout << x << endl; //& - address of operator 
+    cout << &x << endl; //& - address of operator 
+
+    int* ptr = &x; // * denotes a pointer
+    
+    cout << *ptr << endl; //* - denoter 
+
+    //* can be used in multiplicastion, pointer, and denotions//
+
+    x += 6;
+    cout << *ptr << endl;
+
+    *ptr = 213;
+
+    cout << x << endl;
     
     return 0;
 }
